5bd5579f18c273ee6e97ebe40ab499b2751aac74
kamal-shrestha <kamalandshrestha@gmail.com>
2023-03-29 12:21:39 +0545

    function change

---
+++ acm_session/main.py
@@ -0,0 +1,9 @@
+# a function to calculate the number of days between two dates
+
+import datetime
+
+
+def days_between(d1, d2):
+    d1 = datetime.datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.datetime.strptime(d2, "%Y-%m-%d")
+    return abs((d2 - d1).days)
